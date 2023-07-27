2b83d4abe77b5587c090bba393372c9775adec9d
RomanLyashenko <03lroman@gmail.com>
2023-07-27 20:45:34 +0300

    Added class "Main"

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        MyOwnSet<String> amigoSet = new MyOwnSet<>();
+
+        amigoSet.add("Cookies");
+        amigoSet.add("Milk");
+        amigoSet.add("& Fun!");
+
+        System.out.println(amigoSet);
+    }
+}
